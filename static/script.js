97741cb547ab7bb33fee8e84a987557a63f753a1
Nihitha <nsunil@andrew.cmu.edu>
2025-03-16 23:09:43 -0400

    step 7

--- static/script.js
+++ static/script.js
@@ -7,3 +7,30 @@ let A = "Hello ";
 let B = "world!";
 let C = A + B;
 console.log(C);
+
+function SumNPrint(x1, x2) {
+    let x3 = x1 + x2; 
+    console.log(x3); 
+  }
+
+SumNPrint(x, y); 
+SumNPrint(A, B);
+
+if (C.length > z) {
+    console.log(C); 
+  } else if (C.length < z) {
+    console.log(z);
+  } else {
+    console.log("good job!");
+  }
+
+let L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
+let L2 = ["Apple", "Banana", "Kiwi", "Orange"];
+
+function findTheBanana(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === "Banana") {
+        alert("Banana found!");
+      }
+    }
+  }
